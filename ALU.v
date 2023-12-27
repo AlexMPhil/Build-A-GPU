c2184f5e358ffd58f32b58d49653fdc22a600a61
AlexMPhil <97394361+AlexMPhil@users.noreply.github.com>
2023-12-27 13:27:06 +0400

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -10,16 +10,16 @@ assign isZero = (C == 0) ? 1: 0;
 assign isEqual = (A == B) ? 1 : 0;
 assign isGT = (A > B) ? 1 : 0;
 assign isLT = (A < B) ? 1 : 0;
-parameter   Add = 4'd0,
-            Sub = 4'd1,
-            SL = 4'd2,
-            SR = 4'd3,
-            AND = 4'd4,
-            OR = 4'd5,
-            XOR = 4'd6,
-            NAND = 4'd7,
-            NOT = 4'd8,
-            NOR = 4'd9;
+parameter [3:0]  Add = 4'd0,
+                Sub = 4'd1,
+                SL = 4'd2,
+                SR = 4'd3,
+                AND = 4'd4,
+                OR = 4'd5,
+                XOR = 4'd6,
+                NAND = 4'd7,
+                NOT = 4'd8,
+                NOR = 4'd9;
 always @ (ctrl, A, B) begin
     case(ctrl)
     add: begin
